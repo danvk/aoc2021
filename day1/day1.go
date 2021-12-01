134d468f8545d83fb9b9805e8ce464bc725a06c8
Dan Vanderkam <danvdk@gmail.com>
2021-12-01 08:52:54 -0500

    day 1 star 2

--- day1/day1.go
+++ day1/day1.go
@@ -8,6 +8,20 @@ import (
 	"strconv"
 )
 
+func calcSums(nums []int, n int) []int {
+	sum := 0
+	for i := 0; i < n; i++ {
+		sum += nums[i]
+	}
+
+	sums := []int{sum}
+	for i := n; i < len(nums); i++ {
+		sums = append(sums, sums[len(sums)-1]+nums[i]-nums[i-n])
+	}
+
+	return sums
+}
+
 func main() {
 	file, err := os.Open(os.Args[1])
 	if err != nil {
@@ -17,22 +31,32 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	// optionally, resize scanner's capacity for lines over 64K, see next example
-	last := -1
-	numInc := 0
+	nums := make([]int, 0)
 	for scanner.Scan() {
 		line := scanner.Text()
 		this, err := strconv.Atoi(line)
 		if err != nil {
 			log.Fatal(err)
 		}
-		if this > last && last != -1 {
-			numInc += 1
-		}
-		last = this
+		nums = append(nums, this)
+	}
+
+	sums := calcSums(nums, 3)
+	for _, sum := range sums {
+		fmt.Printf("%d\n", sum)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
+	}
+
+	last := -1
+	numInc := 0
+	for _, n := range sums {
+		if n > last && last != -1 {
+			numInc++
+		}
+		last = n
 	}
 
 	fmt.Printf("Num increased: %d\n", numInc)
