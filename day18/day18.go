8fc8e24ab4df336d8ccc1badad805bac6887d939
Dan Vanderkam <danvdk@gmail.com>
2021-12-18 11:41:18 -0500

    Exploding like a pro

--- day18/day18.go
+++ day18/day18.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"aoc/util"
 	"fmt"
 	"os"
 	"regexp"
@@ -20,6 +19,88 @@ func (p Pair) String() string {
 		return fmt.Sprintf("[%s,%s]", p.left.String(), p.right.String())
 	}
 	return strconv.Itoa(p.value)
+}
+
+func (p Pair) Add(other Pair) Pair {
+	return Pair{
+		left:  &p,
+		right: &other,
+	}
+}
+
+func (p Pair) IsValue() bool {
+	return p.left == nil
+}
+
+// Returns new pair, exploding left, exploding right, was there an explosion?
+func (p Pair) Explode(depth int) (Pair, *int, *int, bool) {
+	if p.IsValue() {
+		return p, nil, nil, false
+	}
+	if p.left.IsValue() && p.right.IsValue() && depth >= 4 {
+		// this is a pair of two values; explode it!
+		fmt.Printf("Explode! %s, depth=%d\n", p, depth)
+		return Pair{value: 0}, &p.left.value, &p.right.value, true
+	}
+
+	// Try to explode the left
+	left, expL, expR, exploded := p.left.Explode(depth + 1)
+	if expR != nil && p.right.IsValue() {
+		// There's an explosion! And we can handle the right part.
+		return Pair{left: &left, right: &Pair{value: p.right.value + *expR}}, expL, nil, exploded
+	}
+	if exploded {
+		right := p.right
+		if expR != nil {
+			newRight := right.ExplodeDownLeft(*expR)
+			right = &newRight
+		}
+		return Pair{left: &left, right: right}, expL, expR, exploded
+	}
+
+	right, expL, expR, exploded := p.right.Explode(depth + 1)
+	if expL != nil && p.left.IsValue() {
+		// There's an explosion and we can handle the left part.
+		return Pair{left: &Pair{value: p.left.value + *expL}, right: &right}, nil, expR, exploded
+	}
+	if exploded {
+		left := p.left
+		if expL != nil {
+			newLeft := left.ExplodeDownRight(*expL)
+			left = &newLeft
+		}
+		return Pair{left: left, right: &right}, expL, expR, exploded
+	}
+
+	// No explosions! We're unchanged.
+	return p, nil, nil, false
+}
+
+func (p Pair) ExplodeDownLeft(val int) Pair {
+	if p.IsValue() {
+		return Pair{value: p.value + val}
+	}
+	newLeft := p.left.ExplodeDownLeft(val)
+	return Pair{left: &newLeft, right: p.right}
+}
+
+func (p Pair) ExplodeDownRight(val int) Pair {
+	if p.IsValue() {
+		return Pair{value: p.value + val}
+	}
+	newRight := p.right.ExplodeDownRight(val)
+	return Pair{left: p.left, right: &newRight}
+}
+
+func (p Pair) ReduceOnce() (Pair, bool) {
+	if p.IsValue() {
+		return p, false
+	}
+	np, _, _, exploded := p.Explode(0)
+	if exploded {
+		return np, true
+	}
+	return p, false
 }
 
 var NumPat = regexp.MustCompile("\\d+")
@@ -51,12 +132,16 @@ func ParsePair(text string) (Pair, string) {
 }
 
 func main() {
-	linesText := util.ReadLines(os.Args[1])
+	// linesText := util.ReadLines(os.Args[1])
 
-	for _, line := range linesText {
+	// for _, line := range linesText {
+	for _, line := range []string{os.Args[1]} {
 		pair, rest := ParsePair(line)
 		if len(rest) != 0 {
 			panic(line)
 		}
+
+		np, reduced := pair.ReduceOnce()
+		fmt.Printf("%s -> %s, %v\n", pair, np, reduced)
 	}
 }
---
+++ day18/day18_test.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+func TestExplode(t *testing.T) {
+	p, _ := ParsePair("[3,2]")
+	r, expL, expR, exploded := p.Explode(3)
+	if r.String() != p.String() || expL != nil || expR != nil || exploded {
+		t.Errorf("[3,2].Explode(3) == %s, %v, %v, %v want [3,2], nil, nil, false",
+			r, expL, expR, exploded,
+		)
+	}
+
+	r, expL, expR, exploded = p.Explode(4)
+	if r.String() != "0" || *expL != 3 || *expR != 2 || !exploded {
+		t.Errorf("[3,2].Explode(4) == %s, %v, %v, %v want 0, 3, 2, true",
+			r, expL, expR, exploded,
+		)
+	}
+
+	p, _ = ParsePair("[4,[3,2]]")
+	r, expL, expR, exploded = p.Explode(3)
+	if r.String() != "[7,0]" || expL != nil || expR == nil || *expR != 2 || !exploded {
+		t.Errorf("%s.Explode(3) == %s, %v, %v, %v want [7, 0], nil, 2, true",
+			p, r, expL, expR, exploded,
+		)
+	}
+
+	p, _ = ParsePair("[5,[4,[3,2]]]")
+	r, expL, expR, exploded = p.Explode(2)
+	if r.String() != "[5,[7,0]]" || expL != nil || *expR != 2 || !exploded {
+		t.Errorf("%s.Explode(2) == %s, %v, %v, %v want [5,[7,0]], nil, 2, true",
+			p, r, expL, expR, exploded,
+		)
+	}
+
+}
