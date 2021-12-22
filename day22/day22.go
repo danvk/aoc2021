a4fc18bf8f3aed32d8fab0314a6e29d05cb94b9c
Dan Vanderkam <danvdk@gmail.com>
2021-12-22 12:53:22 -0500

    Intersect / union cubiods; do not think this will scale

--- day22/day22.go
+++ day22/day22.go
@@ -12,7 +12,6 @@ type Coord struct {
 
 type Cuboid struct {
 	x, y, z Interval
-	isOn    bool
 }
 
 func min(x int, y int) int {
@@ -42,6 +41,13 @@ func (iv Interval) IsEmpty() bool {
 	return iv.max > iv.min
 }
 
+func (iv Interval) Length() int {
+	if iv.min <= iv.max {
+		return iv.max - iv.min + 1
+	}
+	return 0
+}
+
 func (a Interval) Intersect(b Interval) Interval {
 	return Interval{
 		min: max(a.min, b.min),
@@ -53,40 +59,123 @@ func (a Interval) Intersects(b Interval) bool {
 	return a.min <= b.max && b.min <= a.max
 }
 
-func (a Interval) Union(b Interval) []Interval {
-	if a.Intersects(b) {
-		return []Interval{{
-			min: min(a.min, b.min),
-			max: max(a.max, b.max),
-		}}
+// Return the parts of a that are not in b
+func (a Interval) Subtract(b Interval) []Interval {
+	if b.max < a.min || b.min > a.max {
+		return []Interval{a}
 	}
-	return []Interval{a, b}
+	if a.min < b.min && a.max > b.max {
+		return []Interval{
+			{a.min, b.min - 1},
+			{b.max + 1, a.max},
+		}
+	}
+	if a.min < b.min {
+		return []Interval{
+			{a.min, b.min - 1},
+		}
+	}
+	if b.max < a.max {
+		return []Interval{
+			{b.max + 1, a.max},
+		}
+	}
+	return []Interval{}
 }
 
-// func IntersectRange(aMin, aMax, bMin, bMax int) int, int {
-//
-// }
+// Returns a-b, a&b, b-a
+func (a Interval) Overlaps(b Interval) []Interval {
+	if !a.Intersects(b) {
+		return []Interval{a, b}
+	}
+	ranges := append(b.Subtract(a), a.Intersect(b))
+	ranges = append(ranges, a.Subtract(b)...)
+	return ranges
+}
 
 func (c Cuboid) Clip() Cuboid {
 	return Cuboid{
-		x:    c.x.Intersect(Interval{-50, 50}),
-		y:    c.y.Intersect(Interval{-50, 50}),
-		z:    c.z.Intersect(Interval{-50, 50}),
-		isOn: c.isOn,
+		x: c.x.Intersect(Interval{-50, 50}),
+		y: c.y.Intersect(Interval{-50, 50}),
+		z: c.z.Intersect(Interval{-50, 50}),
 	}
+}
+
+func (c Cuboid) Intersects(other Cuboid) bool {
+	return c.x.Intersects(other.x) && c.y.Intersects(other.y) && c.z.Intersects(other.z)
+}
+
+func (c Cuboid) Volume() int {
+	return c.x.Length() * c.y.Length() * c.z.Length()
+}
+
+func (c Cuboid) Union(other Cuboid) []Cuboid {
+	var result []Cuboid
+	for _, x := range c.x.Overlaps(other.x) {
+		for _, y := range c.y.Overlaps(other.y) {
+			for _, z := range c.z.Overlaps(other.z) {
+				part := Cuboid{x, y, z}
+				if part.Intersects(c) || part.Intersects(other) {
+					result = append(result, c)
+				}
+			}
+		}
+	}
+	return result
+}
+
+func (c Cuboid) Subtract(other Cuboid) []Cuboid {
+	if !c.Intersects(other) {
+		return []Cuboid{c}
+	}
+
+	var result []Cuboid
+	for _, x := range c.x.Overlaps(other.x) {
+		for _, y := range c.y.Overlaps(other.y) {
+			for _, z := range c.z.Overlaps(other.z) {
+				part := Cuboid{x, y, z}
+				if part.Volume() > 0 && part.Intersects(c) && !part.Intersects(other) {
+					result = append(result, c)
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+// cuboids are disjoint
+func SubtractCuboid(cuboids []Cuboid, sub Cuboid) []Cuboid {
+	var result []Cuboid
+
+	for _, cuboid := range cuboids {
+		if !cuboid.Intersects(sub) {
+			result = append(result, cuboid)
+		} else {
+			result = append(result, cuboid.Subtract(sub)...)
+		}
+	}
+	return result
+}
+
+// cuboids are disjoint
+func AddCuboid(cuboids []Cuboid, add Cuboid) []Cuboid {
+	remains := SubtractCuboid(cuboids, add)
+	remains = append(remains, add)
+	return remains
 }
 
 func Set(c Cuboid, grid map[Coord]bool) {
 	for x := c.x.min; x <= c.x.max; x++ {
 		for y := c.y.min; y <= c.y.max; y++ {
 			for z := c.z.min; z <= c.z.max; z++ {
-				grid[Coord{x, y, z}] = c.isOn
+				grid[Coord{x, y, z}] = true
 			}
 		}
 	}
 }
 
-func ParseLine(line string) Cuboid {
+func ParseLine(line string) (bool, Cuboid) {
 	var c Cuboid
 	var onOff string
 	_, err := fmt.Sscanf(line, "%s x=%d..%d,y=%d..%d,z=%d..%d",
@@ -95,26 +184,28 @@ func ParseLine(line string) Cuboid {
 	if err != nil {
 		panic(err)
 	}
-	if onOff == "on" {
-		c.isOn = true
-	}
-	return c
+	return onOff == "on", c
 }
 
 func main() {
 	linesText := util.ReadLines(os.Args[1])
-	cuboids := util.Map(linesText, ParseLine)
+	var disjointCuboids []Cuboid
 
-	grid := map[Coord]bool{}
-	for _, c := range cuboids {
-		Set(c.Clip(), grid)
+	for i, line := range linesText {
+		isOn, cuboid := ParseLine(line)
+		if isOn {
+			disjointCuboids = AddCuboid(disjointCuboids, cuboid)
+		} else {
+			disjointCuboids = SubtractCuboid(disjointCuboids, cuboid)
+		}
+		disjointCuboids = util.Filter(disjointCuboids, func(c Cuboid) bool { return c.Volume() > 0 })
+
+		fmt.Printf("%d %s -> %d disjoint cuboids\n", i, line, len(disjointCuboids))
 	}
 
 	num := 0
-	for _, v := range grid {
-		if v {
-			num++
-		}
+	for _, cuboid := range disjointCuboids {
+		num += cuboid.Volume()
 	}
 
 	fmt.Printf("Num cells on: %d\n", num)
--- day22/day22_test.go
+++ day22/day22_test.go
@@ -5,8 +5,47 @@ import (
 	"testing"
 )
 
-func add(a, b int) int {
-	return a + b
+func TestSubtractInterval(t *testing.T) {
+	tests := map[string]struct {
+		a    Interval
+		b    Interval
+		want []Interval
+	}{
+		"overlap": {
+			a:    Interval{10, 20},
+			b:    Interval{15, 25},
+			want: []Interval{{10, 14}},
+		},
+		"disjoint": {
+			a:    Interval{10, 20},
+			b:    Interval{25, 35},
+			want: []Interval{{10, 20}},
+		},
+		"overlapB": {
+			a:    Interval{15, 25},
+			b:    Interval{10, 20},
+			want: []Interval{{21, 25}},
+		},
+		"inside": {
+			a:    Interval{5, 25},
+			b:    Interval{10, 20},
+			want: []Interval{{5, 9}, {21, 25}},
+		},
+		"outside": {
+			a:    Interval{10, 20},
+			b:    Interval{5, 25},
+			want: []Interval{},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := tc.a.Subtract(tc.b)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("%v.Subtract(%v) = %v, want %v", tc.a, tc.b, got, tc.want)
+			}
+		})
+	}
 }
 
 func TestIntersect(t *testing.T) {
